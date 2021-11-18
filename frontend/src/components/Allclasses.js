7d0512b10e65c55a8c4308a441467d7e796798a4
DamruRavihara <damruravihara2@gmail.com>
2021-11-18 08:14:31 +0530

    updated

--- frontend/src/components/Allclasses.js
+++ frontend/src/components/Allclasses.js
@@ -148,6 +148,7 @@ export default function Allclasses(){
                          <Link to={"/student/addstudent/" + classroom._id}><button className="btnregister" id="regsubmit">Add Student</button></Link>
                          <Link to={"/student/allstudents/" + classroom._id}><button className="btnregister" id="regsubmit">All Students</button></Link>
                          <Link to={"/student/attendence/" + classroom._id}><button className="btnregister" id="regsubmit">Attendence</button></Link>
+                         <Link to={"/student/paymentstudent/" + classroom._id}><button className="btnregister" id="regsubmit">payment</button></Link>
                          </td>
                          
                         </tr>
